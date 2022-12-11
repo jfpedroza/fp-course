163baca2f25c4d4f9891528262ea29179d1ef597
Jhon Pedroza <jhon@pedroza.me>
2022-12-11 01:34:45 -0500

    Solve Course.Applicative

--- src/Course/Applicative.hs
+++ src/Course/Applicative.hs
@@ -48,14 +48,12 @@ instance Applicative ExactlyOne where
   pure ::
     a ->
     ExactlyOne a
-  pure =
-    error "todo: Course.Applicative pure#instance ExactlyOne"
+  pure = ExactlyOne
   (<*>) ::
     ExactlyOne (a -> b) ->
     ExactlyOne a ->
     ExactlyOne b
-  (<*>) =
-    error "todo: Course.Applicative (<*>)#instance ExactlyOne"
+  (<*>) (ExactlyOne f) = (f <$>)
 
 -- | Insert into a List.
 --
@@ -67,14 +65,12 @@ instance Applicative List where
   pure ::
     a ->
     List a
-  pure =
-    error "todo: Course.Applicative pure#instance List"
+  pure = (:. Nil)
   (<*>) ::
     List (a -> b) ->
     List a ->
     List b
-  (<*>) =
-    error "todo: Course.Apply (<*>)#instance List"
+  (<*>) lf lv = flatMap (<$> lv) lf
 
 -- | Insert into an Optional.
 --
@@ -92,14 +88,12 @@ instance Applicative Optional where
   pure ::
     a ->
     Optional a
-  pure =
-    error "todo: Course.Applicative pure#instance Optional"
+  pure = Full
   (<*>) ::
     Optional (a -> b) ->
     Optional a ->
     Optional b
-  (<*>) =
-    error "todo: Course.Apply (<*>)#instance Optional"
+  (<*>) mf = bindOptional (mf ??)
 
 -- | Insert into a constant function.
 --
@@ -122,15 +116,13 @@ instance Applicative Optional where
 instance Applicative ((->) t) where
   pure ::
     a ->
-    ((->) t a)
-  pure =
-    error "todo: Course.Applicative pure#((->) t)"
+    (->) t a
+  pure = const
   (<*>) ::
-    ((->) t (a -> b)) ->
-    ((->) t a) ->
-    ((->) t b)
-  (<*>) =
-    error "todo: Course.Apply (<*>)#instance ((->) t)"
+    (->) t (a -> b) ->
+    (->) t a ->
+    (->) t b
+  (<*>) fl fr x = fl x (fr x)
 
 -- | Apply a binary function in the environment.
 --
@@ -157,8 +149,7 @@ lift2 ::
   k a ->
   k b ->
   k c
-lift2 =
-  error "todo: Course.Applicative#lift2"
+lift2 f a b = (f <$> a) <*> b
 
 -- | Apply a ternary function in the environment.
 -- /can be written using `lift2` and `(<*>)`./
@@ -190,8 +181,7 @@ lift3 ::
   k b ->
   k c ->
   k d
-lift3 =
-  error "todo: Course.Applicative#lift3"
+lift3 f a b c = lift2 f a b <*> c
 
 -- | Apply a quaternary function in the environment.
 -- /can be written using `lift3` and `(<*>)`./
@@ -224,16 +214,14 @@ lift4 ::
   k c ->
   k d ->
   k e
-lift4 =
-  error "todo: Course.Applicative#lift4"
+lift4 f a b c d = lift3 f a b c <*> d
 
 -- | Apply a nullary function in the environment.
 lift0 ::
   Applicative k =>
   a ->
   k a
-lift0 =
-  error "todo: Course.Applicative#lift0"
+lift0 = pure
 
 -- | Apply a unary function in the environment.
 -- /can be written using `lift0` and `(<*>)`./
@@ -251,8 +239,7 @@ lift1 ::
   (a -> b) ->
   k a ->
   k b
-lift1 =
-  error "todo: Course.Applicative#lift1"
+lift1 f a = lift0 f <*> a
 
 -- | Apply, discarding the value of the first argument.
 -- Pronounced, right apply.
@@ -277,8 +264,7 @@ lift1 =
   k a ->
   k b ->
   k b
-(*>) =
-  error "todo: Course.Applicative#(*>)"
+(*>) a b = (id <$ a) <*> b
 
 -- | Apply, discarding the value of the second argument.
 -- Pronounced, left apply.
@@ -303,8 +289,7 @@ lift1 =
   k b ->
   k a ->
   k b
-(<*) =
-  error "todo: Course.Applicative#(<*)"
+(<*) a b = (const <$> a) <*> b
 
 -- | Sequences a list of structures to a structure of list.
 --
@@ -326,8 +311,8 @@ sequence ::
   Applicative k =>
   List (k a) ->
   k (List a)
-sequence =
-  error "todo: Course.Applicative#sequence"
+sequence Nil = pure Nil
+sequence (h :. t) = lift2 (:.) h $ sequence t
 
 -- | Replicate an effect a given number of times.
 --
@@ -352,8 +337,7 @@ replicateA ::
   Int ->
   k a ->
   k (List a)
-replicateA =
-  error "todo: Course.Applicative#replicateA"
+replicateA n a = sequence $ replicate n a
 
 -- | Filter a list with a predicate that produces an effect.
 --
@@ -379,8 +363,11 @@ filtering ::
   (a -> k Bool) ->
   List a ->
   k (List a)
-filtering =
-  error "todo: Course.Applicative#filtering"
+filtering _f Nil = pure Nil
+filtering f (h :. t) =
+  let maybeCons True t' = h :. t'
+      maybeCons False t' = t'
+  in lift2 maybeCons (f h) $ filtering f t
 
 -----------------------
 -- SUPPORT LIBRARIES --
