4c182b546226e2372ae07d89ac9c0199d2e22c5d
Jhon Pedroza <jhon@pedroza.me>
2022-12-29 04:05:08 -0500

    Solve Course.Cheque

--- src/Course/Cheque.hs
+++ src/Course/Cheque.hs
@@ -188,7 +188,25 @@ data Digit
   | Seven
   | Eight
   | Nine
-  deriving (Eq, Ord)
+  deriving (Eq, Ord, Show)
+
+-- A data type representing one, two or three digits, which may be useful for grouping.
+data Digit3
+  = D1 Digit
+  | D2 Digit Digit
+  | D3 Digit Digit Digit
+  deriving (Eq, Show)
+
+data Dollars a = Dollars (List a) (List a)
+  deriving (Show)
+
+(<$~>) :: (List a -> List b) -> Dollars a -> Dollars b
+(<$~>) f (Dollars whole cents) = Dollars (f whole) (f cents)
+
+infixl 4 <$~>
+
+bimap :: (List a -> List b) -> (List a -> List b) -> Dollars a -> Dollars b
+bimap f g (Dollars whole cents) = Dollars (f whole) (g cents)
 
 showDigit ::
   Digit ->
@@ -213,13 +231,6 @@ showDigit Eight =
   "eight"
 showDigit Nine =
   "nine"
-
--- A data type representing one, two or three digits, which may be useful for grouping.
-data Digit3
-  = D1 Digit
-  | D2 Digit Digit
-  | D3 Digit Digit Digit
-  deriving (Eq)
 
 -- Possibly convert a character to a digit.
 fromChar ::
@@ -247,6 +258,92 @@ fromChar '9' =
   Full Nine
 fromChar _ =
   Empty
+
+readDigits :: Chars -> Dollars Digit
+readDigits str =
+  let zero = Dollars Nil Nil
+      folder (digits, _) '.' = (digits, True)
+      folder (digits, inCents) c = (optional (add digits inCents) digits (fromChar c), inCents)
+      add (Dollars whole cents) False d = Dollars (d :. whole) cents
+      add (Dollars whole cents) True d = Dollars whole (d :. cents)
+   in bimap reverse (take 2 . reverse) $ fst $ foldLeft folder (zero, False) str
+
+addDigit :: Digit -> Digit3 -> Optional Digit3
+addDigit d (D1 a) = Full $ D2 d a
+addDigit d (D2 a b) = Full $ D3 d a b
+addDigit _d (D3 {}) = Empty
+
+groupDigits :: Dollars Digit -> Dollars Digit3
+groupDigits dollars' = group <$~> bimap emptyToZero (rightPad 2) dollars'
+  where
+    group = foldRight folder Nil
+    emptyToZero Nil = pure Zero
+    emptyToZero l = l
+    rightPad n l = l ++ replicate (n - length l) Zero
+    folder d Nil = pure (D1 d)
+    folder d (d3 :. digits) = optional (:. digits) (D1 d :. d3 :. digits) (addDigit d d3)
+
+trimLeftZero :: Digit3 -> Digit3
+trimLeftZero (D2 Zero d) = D1 d
+trimLeftZero (D3 Zero Zero d) = D1 d
+trimLeftZero (D3 Zero d1 d2) = D2 d1 d2
+trimLeftZero d = d
+
+trimLeftZeros :: Dollars Digit3 -> Dollars Digit3
+trimLeftZeros dollars' = map trimLeftZero <$~> dollars'
+
+showTen :: Digit -> Chars
+showTen Zero = error "Shoult not happen"
+showTen One = error "Should not happen"
+showTen Two = "twenty"
+showTen Three = "thirty"
+showTen Four = "forty"
+showTen Five = "fifty"
+showTen Six = "sixty"
+showTen Seven = "seventy"
+showTen Eight = "eighty"
+showTen Nine = "ninety"
+
+showOneTen :: Digit -> Chars
+showOneTen Zero = "ten"
+showOneTen One = "eleven"
+showOneTen Two = "twelve"
+showOneTen Three = "thirteen"
+showOneTen Four = "fourteen"
+showOneTen Five = "fifteen"
+showOneTen Six = "sixteen"
+showOneTen Seven = "seventeen"
+showOneTen Eight = "eighteen"
+showOneTen Nine = "nineteen"
+
+showDigit3 :: Digit3 -> Chars
+showDigit3 (D1 d) = showDigit d
+showDigit3 (D2 Zero d) = showDigit d
+showDigit3 (D2 One d) = showOneTen d
+showDigit3 (D2 d Zero) = showTen d
+showDigit3 (D2 d1 d2) = showTen d1 ++ "-" ++ showDigit d2
+showDigit3 (D3 d Zero Zero) = showDigit d ++ " hundred"
+showDigit3 (D3 d1 d2 d3) = showDigit d1 ++ " hundred and " ++ showDigit3 (D2 d2 d3)
+
+joinBy :: List a -> List (List a) -> List a
+joinBy _sep Nil = Nil
+joinBy _sep (h :. Nil) = h
+joinBy sep (h :. t) = h ++ sep ++ joinBy sep t
+
+humanize :: Dollars Digit3 -> Dollars Char
+humanize dollars' = humanize' <$~> dollars'
+  where
+    humanize' digits =
+      let strDigits = map showDigit3 digits
+          withIllion = zipWith addIllion (reverse strDigits) illion
+       in joinBy " " $ flatten $ reverse withIllion
+    addIllion strDigit "" = pure strDigit
+    addIllion "zero" _ = Nil
+    addIllion strDigit illion' = strDigit :. illion' :. Nil
+
+withSuffixPlural :: Chars -> Chars -> Chars
+withSuffixPlural suffix word@"one" = word ++ " " ++ suffix
+withSuffixPlural suffix word = word ++ " " ++ suffix ++ "s"
 
 -- | Take a numeric value and produce its English output.
 --
@@ -324,5 +421,8 @@ fromChar _ =
 dollars ::
   Chars ->
   Chars
-dollars =
-  error "todo: Course.Cheque#dollars"
+dollars str =
+  let parse = trimLeftZeros . groupDigits . readDigits
+      amount = parse str
+      (Dollars whole cents) = bimap (withSuffixPlural "dollar") (withSuffixPlural "cent") $ humanize amount
+   in whole ++ " and " ++ cents
